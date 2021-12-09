b49ccc51ba4215aded36efb121833bc33ffdd6df
Somdotta07 <jhilick.s@gmail.com>
2021-12-09 13:54:30 +0100

    Final touches

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { FiUser } from 'react-icons/fi';
 
 export default function NavBar() {
   return (
@@ -7,8 +8,8 @@ export default function NavBar() {
       <div className="nav-item">
         <Link to="/"><li className="nav-b">BOOKS</li></Link>
         <Link to="/categories"><li className="nav-c">CATEGORIES</li></Link>
-        {/* <Link to="/"><li className="oval">Profile</li></Link> */}
       </div>
+      <div className="oval"><FiUser /></div>
     </nav>
   );
 }
